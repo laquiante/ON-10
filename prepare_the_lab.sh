9d2164611e32768ff70c2f927ec336252d0addb8
laquiante <33266194+laquiante@users.noreply.github.com>
2021-11-14 07:31:50 +0100

    Create prepare_the_lab.sh

---
+++ prepare_the_lab.sh
@@ -0,0 +1,3 @@
+sudo ansible-playbook -i /home/cumulus/ON-10/inventory/files/hosts ./pb-prepare_cue.yml
+sudo ansible-playbook -i /home/cumulus/ON-10/inventory/files/hosts ./pb-prepare_leafs.yml
+sudo ansible-playbook -i /home/cumulus/ON-10/inventory/files/hosts ./pb-prepare_hosts.yml
