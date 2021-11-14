5b0844a66a72ae1e166d57ba3d19af342a3f20a7
laquiante <33266194+laquiante@users.noreply.github.com>
2021-11-14 08:37:40 +0100

    Update prepare_the_lab.sh

--- prepare_the_lab.sh
+++ prepare_the_lab.sh
@@ -1,3 +1,3 @@
 sudo ansible-playbook -i /home/cumulus/ON-10/inventory/files/hosts ./pb-prepare_cue.yaml
 sudo ansible-playbook -i /home/cumulus/ON-10/inventory/files/hosts ./pb-prepare_leafs.yaml
-sudo ansible-playbook -i /home/cumulus/ON-10/inventory/files/hosts ./step-01/pb-prepare_hosts.yaml
+sudo ansible-playbook -i /home/cumulus/ON-10/inventory/files/hosts ./step-01/prepare_hosts.yaml
