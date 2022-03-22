bf593109d518e8a5348319d26ef76cf1486b2845
laquiante <33266194+laquiante@users.noreply.github.com>
2022-03-22 11:44:25 +0100

    Update step-00.sh

--- step-00.sh
+++ step-00.sh
@@ -1,4 +1,1 @@
-#sudo ansible-playbook -i /home/cumulus/ON-10/inventory/files/hosts ./step-00/prepare_cue.yaml
-#sudo ansible-playbook -i /home/cumulus/ON-10/inventory/files/hosts ./step-00/prepare_leafs.yaml
-#sudo ansible-playbook -i /home/cumulus/ON-10/inventory/files/hosts ./step-00/prepare_hosts.yaml
 sudo ansible-playbook -i /home/cumulus/ON-10/inventory/files/hosts ./step-00/main.yaml
