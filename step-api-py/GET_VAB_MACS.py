bb486f49473619dc4a992201ce18f3a8b954ee8c
laquiante <33266194+laquiante@users.noreply.github.com>
2022-04-09 20:10:57 +0200

    Update GET_VAB_MACS.py

--- step-api-py/GET_VAB_MACS.py
+++ step-api-py/GET_VAB_MACS.py
@@ -3,8 +3,8 @@
 
 
 import requests
-import ast
 import urllib3
+from prettytable import PrettyTable
 
 SWITCH = "172.16.2.254"
 VAB = "br_default"
@@ -12,10 +12,11 @@ VAB = "br_default"
 # nicht schoen aber heute Nachmittag zweckmaessig
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-ergebnis = requests.get('https://'+SWITCH+':8765/cue_v1/bridge/domain/br_default/mac-table', auth=('cumulus','Academy123'), verify=False)
+ergebnis = requests.get('https://'+SWITCH+':8765/cue_v1/bridge/domain/br_default/mac-table', auth=('cumulus','Academy123'), verfiy=False)
 
-print("Die VAB "+VAB+" innerhalb von "+SWITCH+" hat folgende Adressen gelernt")
+myTable=PrettyTable([SWITCH,VAB,"MAC"])
 for key in  ergebnis.json():
         for sub_key in  ergebnis.json()[key]:
                 if sub_key == "mac":
-                        print(ergebnis.json()[key][sub_key])
+                        myTable.add_row([" "," ",ergebnis.json()[key][sub_key]])
+print(myTable)
