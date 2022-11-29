9eec61b0d98d7ee89b13908bb3be7c891aacdeb8
laquiante <33266194+laquiante@users.noreply.github.com>
2022-11-29 09:13:10 +0100

    Update optional-lldp-stp.sh

--- optional-lldp-stp.sh
+++ optional-lldp-stp.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+# ex:ts=4:sw=4:sts=4:et
+# -*- tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
+# ALQ, Wittdün 2022
+# 
+# sudo ./r_unit-03
+
+if [ -f /home/cumulus/ON-01/.additional.conf ]; then
+        . /home/cumulus/ON-01/.additional.conf
+fi
+
+echo
+
+echo
+echo "===         description            ==="
+echo
+echo "preparing leaf1 and leaf2 with two connecitons"
+echo "via swp5-6. This should result in one port blocking"
+echo "Now you can verify if you receive via all ports"
+echo "LLDP messages"
+
+echo
+echo "===            end                ==="
+
 sudo ansible-playbook -i /home/cumulus/ON-10/inventory/files/hosts ./optional-lldp-stp/main.yaml
